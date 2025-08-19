56ef3e9cb5b37eb151c73f2b26b8be35eff92d04
kampunginggrismalut <kampunginggrismalukuutara@gmail.com>
2025-08-19 21:51:33 +0900

    Update script.js

--- landing_page/js/script.js
+++ landing_page/js/script.js
@@ -1,52 +1,3 @@
-// //Toggle class active
-// const navbarNav = document.querySelector(".navbar-nav");
-
-// // ketika klik hamburger menu tampilkan side bar
-// document.querySelector("#hamburger-menu").onclick = () => {
-//   navbarNav.classList.toggle("active");
-// };
-
-// // klik di luar side bar untuk menutup nav
-// const hamburger = document.querySelector("#hamburger-menu");
-// document.addEventListener("click", function (e) {
-//   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-//     navbarNav.classList.remove("active");
-//   }
-// });
-
-// // SLIDESHOW
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides((slideIndex += n));
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   dots[slideIndex - 1].className += " active";
-// }
 feather.replace();
 
 // Mobile Menu Toggle
@@ -123,4 +74,5 @@ document.addEventListener('DOMContentLoaded', () => {
   // Auto-play (optional)
   // setInterval(nextSlide, 5000);
 });
+
 

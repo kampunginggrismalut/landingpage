3565cd7f1e11d9cfc8c229943ff808461912c199
Resty <kampunginggrismalukuutara@gmail.com>
2025-09-06 22:59:20 +0900

    tambahan halaman template pop up dan script js untuk pop up

--- landing_page/js/script.js
+++ landing_page/js/script.js
@@ -31,6 +31,73 @@ document.addEventListener('DOMContentLoaded', function () {
       mobileJadwalIcon.classList.toggle('rotate-180');
     });
   }
+
+  // Fungsi modal untuk pop up
+  const modal = document.getElementById('jadwal-modal');
+  const modalTitle = document.getElementById('modal-title');
+  const scheduleContent = document.querySelector('#schedule-content > div');
+  const modalCloseBtn = document.getElementById('modal-close-btn');
+
+  document.querySelectorAll('.jadwal-kelas').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const kelas = this.getAttribute('data-kelas');
+      showJadwalModal(kelas);
+    });
+  });
+
+  modalCloseBtn.addEventListener('click', hideModal);
+  modal.addEventListener('click', function (e) {
+    if (e.target === modal) {
+      hideModal();
+    }
+  });
+
+  // Fungsi untuk menamppilkan modal jadwal
+  function showJadwalModal(kelas) {
+    modalTitle.textContent = kelas.toUpperCase();
+    scheduleContent.innerHTML = '';
+
+    // cukup tampilkan hari yang memiliki jadwal
+    if (jadwalKelas[kelas]) {
+      for (const [day, time] of Object.entries(jadwalKelas[kelas])) {
+
+        const dayElement = document.createElement('div');
+        dayElement.className = 'schedule-day p-3 text-center';
+
+        const dayName = document.createElement('div');
+        dayName.className = 'font-bold text-blue-700 mb-2';
+        dayName.textContent = day.toUpperCase();
+
+        const timeParts = time.split('');
+        const timeValue = document.createElement('div');
+        timeValue.className = 'text-xl font-semibold text-gray-800 mb-1';
+        timeValue.textContent = timeParts[0];
+
+        const timePeriod = document.createElement('div');
+        timePeriod.className = 'time-badge text-sm inline-block';
+        timePeriod.textContent = timeParts[1];
+
+        dayElement.appendChild(dayName);
+        dayElement.appendChild(timeValue);
+        dayElement.appendChild(timePeriod);
+        scheduleContent.appendChild(dayElement);
+      }
+    }
+    modal.classList.remove('hidden');
+    setTimeout(() => {
+      modal.classList.add('show');
+    }, 10);
+
+    mobileMenu.classList.add('hidden');
+  }
+
+  function hideModal() {
+    modal.classList.remove('show');
+    setTimeout(() => {
+      modal.classList.add('hidden');
+    }, 300);
+  }
 });
 
 //Carousel
